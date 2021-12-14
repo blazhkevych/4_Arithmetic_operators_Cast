053d2af568de6ab106970d2b2ce9fa9ec4a9cebf
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2021-12-14 16:06:48 +0200

    +2, 3 задача

--- task_2/task_2.cpp
+++ task_2/task_2.cpp
@@ -15,5 +15,17 @@ int main()
 	SetConsoleCP(1251); //встановлює потрібну кодову таблицю, на потік введення
 	SetConsoleOutputCP(1251); //встановлює потрібну кодову таблицю, на потік виводу
 
+	cout << "Введите массу в тоннах (дробное число): " << endl;
+	double weight_in_tons = { 0.0 };
+	cin >> weight_in_tons; //126.456789
+
+	int tons = weight_in_tons; //126
+	double kilogram = (weight_in_tons - tons) * 1000; //456.789
+	int gram = (kilogram - (int)kilogram) * 1000; //(456.789-456)*1000=789
+
+	cout << "Количество тонн: " << tons << endl;
+	cout << "Количество килограмм: " << (int)kilogram << endl;
+	cout << "Количество грамм: " << gram << endl;
+
 	return 0;
 }
--- task_3/task_3.cpp
+++ task_3/task_3.cpp
@@ -15,5 +15,33 @@ int main()
 	SetConsoleCP(1251); //встановлює потрібну кодову таблицю, на потік введення
 	SetConsoleOutputCP(1251); //встановлює потрібну кодову таблицю, на потік виводу
 
+	cout << "Введите время начала разговора (в формате часы, минуты и секунды, например \"145512\"):" << endl;
+	int start_of_conversation{ 0 };
+	cin >> start_of_conversation; //145512
+
+	int start_of_conversation_h = start_of_conversation / 10000; //145512/10000=14
+	int start_of_conversation_m = (start_of_conversation - (start_of_conversation_h * 10000)) / 100; //(145512-(14*10000))/100=55
+	int start_of_conversation_s = start_of_conversation_h % 100; //145512%100=12
+
+	cout << "Введите время окончания разговора (в формате часы, минуты и секунды, например \"152056\"):" << endl;
+	int end_of_conversation{ 0 };
+	cin >> end_of_conversation; //152056
+
+	int end_of_conversation_h = end_of_conversation / 10000; //152056/10000=15
+	int end_of_conversation_m = (end_of_conversation - (end_of_conversation_h * 10000)) / 100; //(152056(15*10000))/100=20
+	int end_of_conversation_s = end_of_conversation_h % 100; //152056%100=56
+
+	int call_duration_in_seconds =
+		((end_of_conversation_h * 60 * 60) + (end_of_conversation_m * 60) + end_of_conversation_s)
+		-
+		((start_of_conversation_h * 60 * 60) + (start_of_conversation_m * 60) + start_of_conversation_s);
+	//((15*60*60)+(20*60)+56)-((14*60*60)+(55*60)+12)=54136-53712=424
+
+	const double cost_of_a_minute = 30;
+
+	double total_cost = (call_duration_in_seconds * cost_of_a_minute) / 100;
+
+	cout << "Стоимость разговора составляет: " << total_cost << " грн.";
+
 	return 0;
 }
