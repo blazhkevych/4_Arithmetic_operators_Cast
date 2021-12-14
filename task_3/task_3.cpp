4ffcb1069fb66f58d6a7fdf9ddc95e7c53a1e50b
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2021-12-14 16:23:10 +0200

    исправление ошибки в 3 задаче

--- task_3/task_3.cpp
+++ task_3/task_3.cpp
@@ -21,7 +21,7 @@ int main()
 
 	int start_of_conversation_h = start_of_conversation / 10000; //145512/10000=14
 	int start_of_conversation_m = (start_of_conversation - (start_of_conversation_h * 10000)) / 100; //(145512-(14*10000))/100=55
-	int start_of_conversation_s = start_of_conversation_h % 100; //145512%100=12
+	int start_of_conversation_s = start_of_conversation % 100; //145512%100=12
 
 	cout << "Введите время окончания разговора (в формате часы, минуты и секунды, например \"152056\"):" << endl;
 	int end_of_conversation{ 0 };
@@ -29,17 +29,17 @@ int main()
 
 	int end_of_conversation_h = end_of_conversation / 10000; //152056/10000=15
 	int end_of_conversation_m = (end_of_conversation - (end_of_conversation_h * 10000)) / 100; //(152056(15*10000))/100=20
-	int end_of_conversation_s = end_of_conversation_h % 100; //152056%100=56
+	int end_of_conversation_s = end_of_conversation % 100; //152056%100=56
 
 	int call_duration_in_seconds =
 		((end_of_conversation_h * 60 * 60) + (end_of_conversation_m * 60) + end_of_conversation_s)
 		-
 		((start_of_conversation_h * 60 * 60) + (start_of_conversation_m * 60) + start_of_conversation_s);
-	//((15*60*60)+(20*60)+56)-((14*60*60)+(55*60)+12)=54136-53712=424
+	//((15*60*60)+(20*60)+56)-((14*60*60)+(55*60)+12)=(54000+1200+56)-(50400+3300+12)=55256-53712=1544
 
 	const double cost_of_a_minute = 30;
 
-	double total_cost = (call_duration_in_seconds * cost_of_a_minute) / 100;
+	double total_cost = (call_duration_in_seconds * cost_of_a_minute) / 100; //(1544*30)/100=463,2
 
 	cout << "Стоимость разговора составляет: " << total_cost << " грн.";
 
